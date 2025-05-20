d7f4a0da791dca973adadf63fec3a8144258ce0e
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-05-20 18:14:16 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='Container'>
+      <div className='Search-Section'>
+        <form action="#" className='Search-Form'>
+          <input type="search" className="search-input"placeholder='Enter a city' />
+          
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
